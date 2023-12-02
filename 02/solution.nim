488a9ceab7720ec3724df6b558e68632b7ed1909
Linus Hochbaum <linus@hochbaum.dev>
2023-12-02 20:52:20 +0100

    Submit day 2 part 2

--- 02/solution.nim
+++ 02/solution.nim
@@ -37,13 +37,30 @@ proc isInvalid(game: Game): bool =
         for color, amount in maxCubes:
             if bag.getOrDefault(color, 0) > amount:
                 return true
-    return false
+    
+    result = false
 
-let possibleSum = readFile("./02/input.txt")
-    .splitLines()
-    .map(parseGame)
-    .filter(proc (g: Game): bool = not g.isInvalid())
-    .map(proc (g: Game): int = g.id)
-    .sum()
+proc prod(self: seq[int]): int =
+    for i in self:
+        if result == 0: result = 1
+        result *= i
+
+proc calcPower(game: Game): int =
+    var maxes = initTable[string, int]()
+    for bag in game.bags:
+        for color, amount in bag:
+            maxes[color] = max(maxes.getOrDefault(color, 0), amount)
+            
+    result = maxes.values().toSeq().prod()       
+
+let 
+    games = readFile("./02/input.txt")
+        .splitLines()
+        .map(parseGame)
+    possibleSum = games.filter(proc (g: Game): bool = not g.isInvalid())
+        .map(proc (g: Game): int = g.id)
+        .sum()
+    minPowers = games.map(calcPower).sum()
 
 echo "Part 1 result: ", possibleSum
+echo "Part 2 result: ", minPowers
